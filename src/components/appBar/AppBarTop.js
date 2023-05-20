a83eebd1f089e3981912290927b41bb8cbfd27a6
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2023-05-20 15:35:58 +0200

    Implemented search functionality

--- src/components/appBar/AppBarTop.js
+++ src/components/appBar/AppBarTop.js
@@ -8,61 +8,21 @@ import {
   TextField,
   Toolbar,
 } from "@mui/material";
-import { styled, alpha } from "@mui/material/styles";
 import { Menu, SearchRounded } from "@mui/icons-material";
-import InputBase from "@mui/material/InputBase";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 function AppBarTop(props) {
   const fb = { ...props.value };
-
   const [queryResponse, setQueryResponse] = useState([]);
+
   async function searchQuery(e) {
+    if (e === "") {
+      setQueryResponse([])
+      return
+    }
     const data = await fb.getBlueprintsByNamePartial(e);
-    console.log(data);
     setQueryResponse(data);
   }
+  
   function selectBlueprint(bp) {
     //fb.setViewingBlueprint(bp);
     fb.setCurrentItemByID(bp.products.typeid);
@@ -119,6 +79,7 @@ function AppBarTop(props) {
                 ...params.inputProps,
                 autoComplete: "new-password", // disable autocomplete and autofill
               }}
+              sx={{backgroundColor: "white"}}
               onChange={(event) => {
                 searchQuery(event.target.value);
               }}
